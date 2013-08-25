13a598b632e05d994f0717cb9e6514fdfb7df235
Terry Chen <terryschen@gmail.com>
2013-08-25 14:24:58 -0400

    package.js fix

--- package.js
+++ package.js
@@ -1,8 +1,13 @@
-Package.describe({
-  summary: "jQuery pageslide repackaged for Meteor"
-});
+try {
+  Package.describe({
+    summary: "jQuery pageslide repackaged for Meteor"
+  });
 
-Package.on_use(function (api) {
-  api.use('jquery', 'client');
-  api.add_files('lib/jquery-pageslide/jquery.pageslide.js','client');
-});
+  Package.on_use(function (api) {
+    api.use('jquery', 'client');
+    api.add_files('lib/jquery-pageslide/jquery.pageslide.js','client');
+  });
+}
+catch (err) {
+  console.log("Error while trying to load a package: " + err.message);
+};
