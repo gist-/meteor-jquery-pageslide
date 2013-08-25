da60f8c169068940fd3ce3e01f4303742e96887d
Terry Chen <terryschen@gmail.com>
2013-08-25 14:58:28 -0400

    forgot to add the css

--- package.js
+++ package.js
@@ -6,6 +6,7 @@ try {
   Package.on_use(function (api) {
     api.use('jquery', 'client');
     api.add_files('lib/jquery-pageslide/jquery.pageslide.js','client');
+    api.add_files('lib/jquery-pageslide/jquery.pageslide.css','client');
   });
 }
 catch (err) {
